2570defeb576d9b68fd8468cc9d628b65d366040
chausies <chausies@berkeley.edu>
2015-08-31 23:35:19 +1000

    Updated bitonic_tourney with proper docstring and tests

--- bitonic_tourney.py
+++ bitonic_tourney.py
@@ -1,4 +1,44 @@
 def bitonic_comps(k):
+  """Returns the matches to play for a Bitnoic Tourney (An effecient
+  tourney that completely sorts competitors)
+
+  Given `2^k` competitors, returns `R` rounds with `n/2` matches each to
+  play in order to not only find 1st place, but assign places to everyone.
+  The number of rounds `R` is `k*(k-1)/2`, which is insanely effecient. 
+
+  For every match `(i, j)`, competitors `i` and `j` fight. If `j` wins, then
+  `j` and `i` swap ranks (competitor `i` will now be `j`, competitor `j`
+  will now be `i`).
+
+  Parameters
+  ----------
+  k : positive int
+    `2^k` is the number of competitors in the tourney.
+
+  Returns
+  -------
+  comps : list of lists of tuples
+    `comps` is a list of `round`'s
+    `round`'s are a lists of `match`'s
+    `match`'s are tuples of the form (i, j), where `i` and `j` are the
+        current ranks of the competitors to fight.
+
+  Example
+  -------
+  Hold a bitonic tourney with 8 competitors
+  >>> comps = bitonic_comps(3)
+  >>> competitors = [0, 7, 3, 6, 5, 1, 2, 4]
+  >>> for rnd in comps:
+  ...   for match in rnd:
+  ...     i, j = match[0], match[1]
+  ...     if competitors[i] > competitors[j]:
+  ...       # swap the places (current ranks) of the competitors if 
+  ...       # they are in the incorrect order
+  ...       competitors[i], competitors[j] = competitors[j], competitors[i]
+  >>> print competitors
+  [0, 1, 2, 3, 4, 5, 6, 7]
+  """
+
   if k == 0:
     return []
   comps = []
@@ -22,16 +62,3 @@ def bitonic_comps(k):
     comps.append(curr_r)
     s /= 2
   return comps
-
-from pprint import pprint
-from pylab import permutation
-perm = lambda n : list(permutation(n))
-k = 4
-a = perm(range(2**k))
-comps = bitonic_comps(k)
-pprint(comps)
-for r in comps:
-  for g in r:
-    if a[g[0]] > a[g[1]]:
-      a[g[0]], a[g[1]] = a[g[1]], a[g[0]]
-print a
